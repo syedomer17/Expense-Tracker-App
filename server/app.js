f8dec1862cc737e4a96b797ce31daa2ab376b56f
syedomer17 <syedomerali2006@gmail.com>
2026-05-09 12:42:09 +0530

    old backend

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: env.CLIENT_URL || "http://localhost:5173",
+    origin: [env.CLIENT_URL || "http://localhost:5173","http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
--- server/limits/rateLimit.js
+++ server/limits/rateLimit.js
@@ -4,7 +4,7 @@ import rateLimit from 'express-rate-limit';
 // Apply rate limiting to all requests
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 500, // Limit each IP to 100 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
